2845151fccd8c59de926c5d926d723c75fc40ac6
IchHabeHunger54 <kingdavidaut@gmail.com>
2025-08-19 12:47:02 +0200

    add config translations

--- src/api/java/at/minecraftschurli/arsmagicalegacy/api/constants/AMTranslations.java
+++ src/api/java/at/minecraftschurli/arsmagicalegacy/api/constants/AMTranslations.java
@@ -3,7 +3,7 @@ package at.minecraftschurli.arsmagicalegacy.api.constants;
 import at.minecraftschurli.arsmagicalegacy.api.ArsMagicaApi;
 
 public interface AMTranslations {
-    String CONFIG = "config." + ArsMagicaApi.MOD_ID + ".";
+    String CONFIG = ArsMagicaApi.MOD_ID + ".configuration.";
     String SPELL_CAST_BURNED_OUT = ArsMagicaApi.MOD_ID + ".spell_cast.burned_out";
     String SPELL_CAST_MALFORMED = ArsMagicaApi.MOD_ID + ".spell_cast.malformed";
     String SPELL_CAST_NOT_ENOUGH_MANA = ArsMagicaApi.MOD_ID + ".spell_cast.not_enough_mana";

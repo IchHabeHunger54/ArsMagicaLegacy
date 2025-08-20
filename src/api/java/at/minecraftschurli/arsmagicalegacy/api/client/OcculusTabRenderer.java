19c26b6b3512573b139c1a47fa9cb9fc1e8d1658
David Neustifter <e12225088@student.tuwien.ac.at>
2025-08-20 16:41:48 +0200

    occulus tab background rendering

--- src/api/java/at/minecraftschurli/arsmagicalegacy/api/client/OcculusTabRenderer.java
+++ src/api/java/at/minecraftschurli/arsmagicalegacy/api/client/OcculusTabRenderer.java
@@ -1,18 +1,27 @@
 package at.minecraftschurli.arsmagicalegacy.api.client;
 
 import at.minecraftschurli.arsmagicalegacy.api.magic.OcculusTab;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Renderable;
+import net.minecraft.resources.ResourceLocation;
 
 public abstract class OcculusTabRenderer implements Renderable {
     public static final int TAB_SIZE = 196;
-    private final OcculusTab tab;
+    protected final OcculusTab tab;
+    protected final ResourceLocation tabId;
 
-    public OcculusTabRenderer(OcculusTab tab) {
+    public OcculusTabRenderer(OcculusTab tab, ResourceLocation tabId) {
         this.tab = tab;
+        this.tabId = tabId;
+    }
+
+    @Override
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        guiGraphics.blit(OcculusTab.background(tabId), 0, 0, 0, 0, TAB_SIZE, TAB_SIZE, TAB_SIZE, TAB_SIZE);
     }
 
     @FunctionalInterface
     public interface Factory {
-        OcculusTabRenderer create(OcculusTab tab);
+        OcculusTabRenderer create(OcculusTab tab, ResourceLocation tabId);
     }
 }
--- src/main/java/at/minecraftschurli/arsmagicalegacy/client/screen/occulus/AffinityTabRenderer.java
+++ src/main/java/at/minecraftschurli/arsmagicalegacy/client/screen/occulus/AffinityTabRenderer.java
@@ -3,14 +3,16 @@ package at.minecraftschurli.arsmagicalegacy.client.screen.occulus;
 import at.minecraftschurli.arsmagicalegacy.api.client.OcculusTabRenderer;
 import at.minecraftschurli.arsmagicalegacy.api.magic.OcculusTab;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.resources.ResourceLocation;
 
 public class AffinityTabRenderer extends OcculusTabRenderer {
-    public AffinityTabRenderer(OcculusTab tab) {
-        super(tab);
+    public AffinityTabRenderer(OcculusTab tab, ResourceLocation tabId) {
+        super(tab, tabId);
     }
 
     @Override
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        super.render(guiGraphics, mouseX, mouseY, partialTick);
         //TODO
     }
 }

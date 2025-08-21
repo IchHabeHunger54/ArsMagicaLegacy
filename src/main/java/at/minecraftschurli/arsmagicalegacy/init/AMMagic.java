9482b1489dee53f8d31d98266aa18bd78e15dd8f
David Neustifter <e12225088@student.tuwien.ac.at>
2025-08-21 16:39:34 +0200

    add skill translations and comment out unused skills

--- src/main/java/at/minecraftschurli/arsmagicalegacy/init/AMMagic.java
+++ src/main/java/at/minecraftschurli/arsmagicalegacy/init/AMMagic.java
@@ -19,9 +19,13 @@ public interface AMMagic {
     ResourceKey<SkillPoint> RED_POINT                 = skillPoint("red");
     ResourceKey<Skill>      AFFINITY_GAINS_BOOST      = skill("affinity_gains_boost");
     ResourceKey<Skill>      AUGMENTED_CASTING         = skill("augmented_casting");
+    ResourceKey<Skill>      EXTRA_SUMMONS             = skill("extra_summons");
+    ResourceKey<Skill>      MAGE_BAND_1               = skill("mage_band_1");
+    ResourceKey<Skill>      MAGE_BAND_2               = skill("mage_band_2");
     ResourceKey<Skill>      MANA_REGENERATION_BOOST_1 = skill("mana_regeneration_boost_1");
     ResourceKey<Skill>      MANA_REGENERATION_BOOST_2 = skill("mana_regeneration_boost_2");
     ResourceKey<Skill>      MANA_REGENERATION_BOOST_3 = skill("mana_regeneration_boost_3");
+    ResourceKey<Skill>      SHIELD_OVERLOAD           = skill("shield_overload");
     ResourceKey<Skill>      SPELL_MOTION              = skill("spell_motion");
 
     private static ResourceKey<OcculusTab> occulusTab(String name) {

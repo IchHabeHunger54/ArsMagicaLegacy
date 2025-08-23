9df8f1bfbde566d5ecea11fb0999741ee3d5cf47
IchHabeHunger54 <kingdavidaut@gmail.com>
2025-08-23 22:35:35 +0200

    add skill and skill point storage on the player

--- src/api/java/at/minecraftschurli/arsmagicalegacy/api/helper/MagicHelper.java
+++ src/api/java/at/minecraftschurli/arsmagicalegacy/api/helper/MagicHelper.java
@@ -1,5 +1,8 @@
 package at.minecraftschurli.arsmagicalegacy.api.helper;
 
+import at.minecraftschurli.arsmagicalegacy.api.magic.Skill;
+import at.minecraftschurli.arsmagicalegacy.api.magic.SkillPoint;
+import net.minecraft.core.Holder;
 import net.minecraft.world.entity.player.Player;
 
 public interface MagicHelper {
@@ -20,4 +23,26 @@ public interface MagicHelper {
     boolean knowsMagic(Player player);
 
     void initiateMagic(Player player);
+
+    boolean knows(Player player, Holder<Skill> skill);
+
+    boolean canLearn(Player player, Holder<Skill> skill);
+
+    void learn(Player player, Holder<Skill> skill);
+
+    void forget(Player player, Holder<Skill> skill);
+
+    void learnAll(Player player);
+
+    void forgetAll(Player player);
+
+    int getSkillPoint(Player player, Holder<SkillPoint> skillPoint);
+
+    void addSkillPoint(Player player, Holder<SkillPoint> skillPoint, int amount);
+
+    void addSkillPoint(Player player, Holder<SkillPoint> skillPoint);
+
+    void removeSkillPoint(Player player, Holder<SkillPoint> skillPoint, int amount);
+
+    void removeSkillPoint(Player player, Holder<SkillPoint> skillPoint);
 }
--- src/main/java/at/minecraftschurli/arsmagicalegacy/init/AMAttachments.java
+++ src/main/java/at/minecraftschurli/arsmagicalegacy/init/AMAttachments.java
@@ -1,6 +1,6 @@
 package at.minecraftschurli.arsmagicalegacy.init;
 
-import at.minecraftschurli.arsmagicalegacy.api.magic.MagicLevel;
+import at.minecraftschurli.arsmagicalegacy.api.magic.MagicAttachment;
 import com.mojang.serialization.Codec;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
@@ -11,9 +11,9 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import java.util.function.Supplier;
 
 public interface AMAttachments {
-    DeferredHolder<AttachmentType<?>, AttachmentType<Double>>     BURNOUT     = register("burnout",     () -> 0.,                 Codec.DOUBLE,     ByteBufCodecs.DOUBLE);
-    DeferredHolder<AttachmentType<?>, AttachmentType<MagicLevel>> MAGIC_LEVEL = register("magic_level", () -> MagicLevel.DEFAULT, MagicLevel.CODEC, MagicLevel.STREAM_CODEC);
-    DeferredHolder<AttachmentType<?>, AttachmentType<Double>>     MANA        = register("mana",        () -> 0.,                 Codec.DOUBLE,     ByteBufCodecs.DOUBLE);
+    DeferredHolder<AttachmentType<?>, AttachmentType<Double>>          BURNOUT = register("burnout", () -> 0.,                      Codec.DOUBLE,          ByteBufCodecs.DOUBLE);
+    DeferredHolder<AttachmentType<?>, AttachmentType<MagicAttachment>> MAGIC   = register("magic",   () -> MagicAttachment.DEFAULT, MagicAttachment.CODEC, MagicAttachment.STREAM_CODEC);
+    DeferredHolder<AttachmentType<?>, AttachmentType<Double>>          MANA    = register("mana",    () -> 0.,                      Codec.DOUBLE,          ByteBufCodecs.DOUBLE);
 
     private static <T> DeferredHolder<AttachmentType<?>, AttachmentType<T>> register(String name, Supplier<T> defaultValueSupplier, Codec<T> codec, StreamCodec<? super RegistryFriendlyByteBuf, T> streamCodec) {
         return AMRegistries.ATTACHMENTS.register(name, () -> AttachmentType.builder(defaultValueSupplier).serialize(codec).sync(streamCodec).copyOnDeath().build());

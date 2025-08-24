784d0870745847e91d0f628aac8f7c54411e980e
IchHabeHunger54 <kingdavidaut@gmail.com>
2025-08-24 15:25:49 +0200

    learning skills now costs skill points

--- src/main/java/at/minecraftschurli/arsmagicalegacy/packet/LearnSkillPacket.java
+++ src/main/java/at/minecraftschurli/arsmagicalegacy/packet/LearnSkillPacket.java
@@ -2,12 +2,14 @@ package at.minecraftschurli.arsmagicalegacy.packet;
 
 import at.minecraftschurli.arsmagicalegacy.api.ArsMagicaApi;
 import at.minecraftschurli.arsmagicalegacy.api.constants.AMRegistryKeys;
+import at.minecraftschurli.arsmagicalegacy.api.helper.MagicHelper;
 import at.minecraftschurli.arsmagicalegacy.api.magic.Skill;
 import net.minecraft.core.Holder;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.world.entity.player.Player;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 
 public record LearnSkillPacket(Holder<Skill> skill) implements CustomPacketPayload {
@@ -22,6 +24,13 @@ public record LearnSkillPacket(Holder<Skill> skill) implements CustomPacketPaylo
     }
 
     public void handle(IPayloadContext context) {
-        ArsMagicaApi.getMagicHelper().learn(context.player(), skill);
+        MagicHelper helper = ArsMagicaApi.getMagicHelper();
+        Player player = context.player();
+        if (helper.canLearn(player, skill)) {
+            helper.learn(player, skill);
+            if (!player.isCreative()) {
+                skill.value().cost().ifPresent(cost -> helper.removeSkillPoint(player, cost));
+            }
+        }
     }
 }

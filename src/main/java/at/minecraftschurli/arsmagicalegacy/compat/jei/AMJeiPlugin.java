8b7d27599c5c40e63bd76a15ec81f2312a6a8810
IchHabeHunger54 <kingdavidaut@gmail.com>
2025-08-28 21:17:41 +0200

    add jei plugin

---
+++ src/main/java/at/minecraftschurli/arsmagicalegacy/compat/jei/AMJeiPlugin.java
@@ -0,0 +1,24 @@
+package at.minecraftschurli.arsmagicalegacy.compat.jei;
+
+import at.minecraftschurli.arsmagicalegacy.api.ArsMagicaApi;
+import at.minecraftschurli.arsmagicalegacy.init.AMItems;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.registration.ISubtypeRegistration;
+import net.minecraft.resources.ResourceLocation;
+
+@JeiPlugin
+public final class AMJeiPlugin implements IModPlugin {
+    private static final ResourceLocation ID = ArsMagicaApi.modLoc(ArsMagicaApi.MOD_ID);
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return ID;
+    }
+
+    @Override
+    public void registerItemSubtypes(ISubtypeRegistration registration) {
+        registration.registerSubtypeInterpreter(VanillaTypes.ITEM_STACK, AMItems.INFINITY_ORB.get(), SkillPointSubtypeInterpreter.INSTANCE);
+    }
+}

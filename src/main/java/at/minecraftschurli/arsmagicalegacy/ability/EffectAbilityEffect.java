cdc939259a58cbef97a996e00591277c3ff5f1ab
IchHabeHunger54 <kingdavidaut@gmail.com>
2025-09-12 03:36:06 +0200

    improve serialization of some ability effects

--- src/main/java/at/minecraftschurli/arsmagicalegacy/ability/EffectAbilityEffect.java
+++ src/main/java/at/minecraftschurli/arsmagicalegacy/ability/EffectAbilityEffect.java
@@ -2,15 +2,20 @@ package at.minecraftschurli.arsmagicalegacy.ability;
 
 import at.minecraftschurli.arsmagicalegacy.api.ability.Ability;
 import at.minecraftschurli.arsmagicalegacy.api.ability.AbilityEffect;
+import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.Holder;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.player.Player;
 
-public record EffectAbilityEffect(MobEffectInstance effect) implements AbilityEffect {
+public record EffectAbilityEffect(Holder<MobEffect> effect, int duration, int amplifier, boolean visible) implements AbilityEffect {
     public static final MapCodec<EffectAbilityEffect> CODEC = RecordCodecBuilder.mapCodec(inst -> inst.group(
-        MobEffectInstance.CODEC.fieldOf("effect").forGetter(EffectAbilityEffect::effect)
+        MobEffect.CODEC.fieldOf("effect").forGetter(EffectAbilityEffect::effect),
+        Codec.INT.fieldOf("duration").forGetter(EffectAbilityEffect::duration),
+        Codec.INT.optionalFieldOf("amplifier", 0).forGetter(EffectAbilityEffect::amplifier),
+        Codec.BOOL.optionalFieldOf("visible", false).forGetter(EffectAbilityEffect::visible)
     ).apply(inst, EffectAbilityEffect::new));
 
     @Override
@@ -20,6 +25,6 @@ public record EffectAbilityEffect(MobEffectInstance effect) implements AbilityEf
 
     @Override
     public void tick(Player player, Holder<Ability> ability) {
-        player.addEffect(new MobEffectInstance(effect));
+        player.addEffect(new MobEffectInstance(effect, duration, amplifier, false, visible, visible));
     }
 }

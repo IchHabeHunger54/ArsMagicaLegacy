424b3aa5138be474580f5ed89af411dd033f9e56
IchHabeHunger54 <kingdavidaut@gmail.com>
2025-09-12 03:36:06 +0200

    improve serialization of some ability effects

--- src/main/java/at/minecraftschurli/arsmagicalegacy/ability/KillEffectAbilityEffect.java
+++ src/main/java/at/minecraftschurli/arsmagicalegacy/ability/KillEffectAbilityEffect.java
@@ -12,6 +12,7 @@ import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.HolderSetCodec;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
@@ -19,11 +20,13 @@ import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
 
 import java.util.Optional;
 
-public record KillEffectAbilityEffect(double min, double max, MobEffectInstance effect, Optional<HolderSet<EntityType<?>>> entities) implements EventTriggeredAbilityEffect<LivingDeathEvent> {
+public record KillEffectAbilityEffect(Holder<MobEffect> effect, double min, double max, int amplifier, boolean visible, Optional<HolderSet<EntityType<?>>> entities) implements EventTriggeredAbilityEffect<LivingDeathEvent> {
     public static final MapCodec<KillEffectAbilityEffect> CODEC = RecordCodecBuilder.mapCodec(inst -> inst.group(
+        MobEffect.CODEC.fieldOf("effect").forGetter(KillEffectAbilityEffect::effect),
         Codec.DOUBLE.fieldOf("min").forGetter(KillEffectAbilityEffect::min),
         Codec.DOUBLE.fieldOf("max").forGetter(KillEffectAbilityEffect::max),
-        MobEffectInstance.CODEC.fieldOf("effect").forGetter(KillEffectAbilityEffect::effect),
+        Codec.INT.optionalFieldOf("amplifier", 0).forGetter(KillEffectAbilityEffect::amplifier),
+        Codec.BOOL.optionalFieldOf("visible", false).forGetter(KillEffectAbilityEffect::visible),
         HolderSetCodec.create(Registries.ENTITY_TYPE, BuiltInRegistries.ENTITY_TYPE.holderByNameCodec(), false).optionalFieldOf("entities").forGetter(KillEffectAbilityEffect::entities)
     ).apply(inst, KillEffectAbilityEffect::new));
 
@@ -36,6 +39,6 @@ public record KillEffectAbilityEffect(double min, double max, MobEffectInstance 
     @Override
     public void apply(LivingDeathEvent event, Player player, Holder<Ability> ability) {
         if (entities.isPresent() && entities.get().contains(event.getEntity().getType().builtInRegistryHolder())) return;
-        player.addEffect(new MobEffectInstance(effect.getEffect(), (int) ArsMagicaApi.abilityHelper().scaleToDepth(player, ability.value(), min, max), effect.getDuration(), effect.isAmbient(), effect.isVisible(), effect.showIcon()));
+        player.addEffect(new MobEffectInstance(effect, (int) ArsMagicaApi.abilityHelper().scaleToDepth(player, ability.value(), min, max), amplifier, false, visible, visible));
     }
 }
--- src/main/java/at/minecraftschurli/arsmagicalegacy/ability/SpellCastEffectAbilityEffect.java
+++ src/main/java/at/minecraftschurli/arsmagicalegacy/ability/SpellCastEffectAbilityEffect.java
@@ -9,12 +9,16 @@ import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.Holder;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.player.Player;
 
-public record SpellCastEffectAbilityEffect(MobEffectInstance effect, double chance) implements EventTriggeredAbilityEffect<SpellCastEvent.Post> {
+public record SpellCastEffectAbilityEffect(Holder<MobEffect> effect, int duration, int amplifier, boolean visible, double chance) implements EventTriggeredAbilityEffect<SpellCastEvent.Post> {
     public static final MapCodec<SpellCastEffectAbilityEffect> CODEC = RecordCodecBuilder.mapCodec(inst -> inst.group(
-        MobEffectInstance.CODEC.fieldOf("effect").forGetter(SpellCastEffectAbilityEffect::effect),
+        MobEffect.CODEC.fieldOf("effect").forGetter(SpellCastEffectAbilityEffect::effect),
+        Codec.INT.fieldOf("duration").forGetter(SpellCastEffectAbilityEffect::duration),
+        Codec.INT.optionalFieldOf("amplifier", 0).forGetter(SpellCastEffectAbilityEffect::amplifier),
+        Codec.BOOL.optionalFieldOf("visible", false).forGetter(SpellCastEffectAbilityEffect::visible),
         Codec.DOUBLE.fieldOf("chance").forGetter(SpellCastEffectAbilityEffect::chance)
     ).apply(inst, SpellCastEffectAbilityEffect::new));
 
@@ -26,7 +30,7 @@ public record SpellCastEffectAbilityEffect(MobEffectInstance effect, double chan
     @Override
     public void apply(SpellCastEvent.Post event, Player player, Holder<Ability> ability) {
         if (player instanceof ServerPlayer serverPlayer && serverPlayer.getRandom().nextDouble() < chance) {
-            player.addEffect(new MobEffectInstance(effect));
+            player.addEffect(new MobEffectInstance(effect, duration, amplifier, false, visible, visible));
         }
     }
 }

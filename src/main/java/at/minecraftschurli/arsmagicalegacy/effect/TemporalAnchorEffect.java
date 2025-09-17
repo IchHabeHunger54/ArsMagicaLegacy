fd20d39bf4a6e2bf3728e00ad6be430b99f08a1e
IchHabeHunger54 <kingdavidaut@gmail.com>
2025-09-17 23:29:17 +0200

    optimize imports

--- src/main/java/at/minecraftschurli/arsmagicalegacy/effect/TemporalAnchorEffect.java
+++ src/main/java/at/minecraftschurli/arsmagicalegacy/effect/TemporalAnchorEffect.java
@@ -11,7 +11,6 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.food.FoodData;
 import net.minecraft.world.phys.Vec3;
 

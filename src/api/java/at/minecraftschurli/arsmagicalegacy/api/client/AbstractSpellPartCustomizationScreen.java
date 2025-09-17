843d959297d3cbe59053f2345ae5573888a85278
IchHabeHunger54 <kingdavidaut@gmail.com>
2025-09-18 01:42:41 +0200

    document SpellPartCustomizationScreen and friends

--- src/api/java/at/minecraftschurli/arsmagicalegacy/api/client/AbstractSpellPartCustomizationScreen.java
+++ src/api/java/at/minecraftschurli/arsmagicalegacy/api/client/AbstractSpellPartCustomizationScreen.java
@@ -7,16 +7,29 @@ import net.minecraft.network.chat.Component;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
+/**
+ * Dummy implementation of {@link SpellPartCustomizationScreen}. This implementation, and as such also {@link SpellPartCustomizationScreen.Factory}, expects the screen to modify exactly one data component.
+ * It is encouraged to follow that pattern. If you need to store multiple values for a single spell part, adjust the type of the data component to accommodate multiple values.
+ * To modify the data component value, change {@link AbstractSpellPartCustomizationScreen#value}. When the screen is closed, the change will be pushed to the parent screen automatically.
+ *
+ * @param <T> The type of the modified data component.
+ */
 public abstract class AbstractSpellPartCustomizationScreen<T> extends Screen implements SpellPartCustomizationScreen {
     private final DataComponentType<T> type;
     private final BiConsumer<DataComponentType<T>, T> setter;
     protected T value;
 
-    public AbstractSpellPartCustomizationScreen(Component title, DataComponentType<T> type, Function<DataComponentType<T>, T> value, BiConsumer<DataComponentType<T>, T> setter) {
+    /**
+     * @param title       The title of the screen.
+     * @param type        The {@link DataComponentType} to use.
+     * @param valueGetter A {@link Function} that extracts the data component value, for initial storage.
+     * @param valueSetter A {@link BiConsumer} that is called when the screen is closed, and is responsible for returning the data component value to the parent screen.
+     */
+    public AbstractSpellPartCustomizationScreen(Component title, DataComponentType<T> type, Function<DataComponentType<T>, T> valueGetter, BiConsumer<DataComponentType<T>, T> valueSetter) {
         super(title);
         this.type = type;
-        this.value = value.apply(type);
-        this.setter = setter;
+        this.value = valueGetter.apply(type);
+        this.setter = valueSetter;
     }
 
     @Override
--- src/api/java/at/minecraftschurli/arsmagicalegacy/api/client/SpellPartCustomizationScreen.java
+++ src/api/java/at/minecraftschurli/arsmagicalegacy/api/client/SpellPartCustomizationScreen.java
@@ -1,12 +1,25 @@
 package at.minecraftschurli.arsmagicalegacy.api.client;
 
+import at.minecraftschurli.arsmagicalegacy.api.spell.SpellPart;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.core.component.DataComponentType;
 
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
+/**
+ * Represents a screen for customizing a {@link SpellPart}.
+ * Implementers are expected to extend {@link Screen}, however this is left as a marker interface to allow other screen classes to be used, e.g. {@link AbstractContainerScreen}.
+ * See {@link AbstractSpellPartCustomizationScreen} for a dummy implementation.
+ */
 public interface SpellPartCustomizationScreen {
+    /**
+     * The factory interface for {@link SpellPartCustomizationScreen}s, used in {@link RegisterSpellPartCustomizationScreensEvent}.
+     *
+     * @param <T> A {@link Function} to get the initial value.
+     * @param <S> A {@link BiConsumer} to set the value when the screen is closed.
+     */
     @FunctionalInterface
     interface Factory<T, S extends Screen & SpellPartCustomizationScreen> {
         S create(Function<DataComponentType<T>, T> valueGetter, BiConsumer<DataComponentType<T>, T> valueSetter);

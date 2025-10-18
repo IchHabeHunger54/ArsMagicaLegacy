06ebfaca45e31ed377002e2fa8ac2c085634fb72
IchHabeHunger54 <e12225088@student.tuwien.ac.at>
2025-10-18 13:56:08 +0200

    add etherium capability (untested)

--- src/main/java/at/minecraftschurli/arsmagicalegacy/block/obelisk/ObeliskBlockEntity.java
+++ src/main/java/at/minecraftschurli/arsmagicalegacy/block/obelisk/ObeliskBlockEntity.java
@@ -1,9 +1,15 @@
 package at.minecraftschurli.arsmagicalegacy.block.obelisk;
 
+import at.minecraftschurli.arsmagicalegacy.AMServerConfig;
 import at.minecraftschurli.arsmagicalegacy.api.constants.AMTranslations;
+import at.minecraftschurli.arsmagicalegacy.api.etherium.EtheriumHandler;
+import at.minecraftschurli.arsmagicalegacy.api.etherium.EtheriumType;
+import at.minecraftschurli.arsmagicalegacy.api.etherium.ObeliskFuel;
 import at.minecraftschurli.arsmagicalegacy.init.AMBlockEntities;
+import at.minecraftschurli.arsmagicalegacy.init.AMEtheriumTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.NonNullList;
 import net.minecraft.core.component.DataComponentMap;
@@ -22,6 +28,7 @@ import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.StackedContentsCompatible;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.ItemContainerContents;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -29,24 +36,59 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class ObeliskBlockEntity extends BlockEntity implements MenuProvider, Nameable, StackedContentsCompatible, WorldlyContainer {
+public class ObeliskBlockEntity extends BlockEntity implements MenuProvider, Nameable, StackedContentsCompatible, WorldlyContainer, EtheriumHandler {
     private static final int[] SLOTS = new int[]{0};
     private LockCode lockCode = LockCode.NO_LOCK;
     private ItemStack stack = ItemStack.EMPTY;
     private Component name;
+    private int etherium;
+    private int burnTime;
+    private int maxBurnTime;
+    private int etheriumPerTick;
 
-    public ObeliskBlockEntity(BlockPos pos, BlockState blockState) {
-        super(AMBlockEntities.OBELISK.get(), pos, blockState);
+    public ObeliskBlockEntity(BlockPos pos, BlockState state) {
+        super(AMBlockEntities.OBELISK.get(), pos, state);
+    }
+
+    public void tick(Level level, BlockPos pos, BlockState state) {
+        if (burnTime > 0) {
+            etherium += etheriumPerTick;
+            burnTime--;
+            setChanged();
+        }
+        if (burnTime <= 0) {
+            maxBurnTime = 0;
+            ObeliskFuel fuel = ObeliskFuel.getFuel(stack);
+            if (fuel != null) {
+                burnTime = fuel.burnTime();
+                maxBurnTime = fuel.burnTime();
+                etheriumPerTick = fuel.etheriumPerTick();
+                if (stack.hasCraftingRemainingItem()) {
+                    stack = stack.getCraftingRemainingItem();
+                } else {
+                    stack.shrink(1);
+                }
+                setChanged();
+            }
+        }
+        boolean lit = burnTime > 0;
+        if (state.getValue(ObeliskBlock.LIT) != lit) {
+            level.setBlockAndUpdate(pos, state.setValue(ObeliskBlock.LIT, lit));
+        }
     }
 
     @Override
     protected void loadAdditional(CompoundTag tag, HolderLookup.Provider registries) {
         super.loadAdditional(tag, registries);
         lockCode = LockCode.fromTag(tag);
-        if (tag.contains("CustomName", 8)) {
+        if (tag.contains("CustomName", CompoundTag.TAG_STRING)) {
             name = parseCustomNameSafe(tag.getString("CustomName"), registries);
         }
         stack = ItemStack.parseOptional(registries, tag.getCompound("Item"));
+        etherium = tag.getInt("Etherium");
+        burnTime = tag.getInt("BurnTime");
+        maxBurnTime = tag.getInt("MaxBurnTime");
+        etheriumPerTick = tag.getInt("EtheriumPerTick");
     }
 
     @Override
@@ -56,7 +98,13 @@ public class ObeliskBlockEntity extends BlockEntity implements MenuProvider, Nam
         if (name != null) {
             tag.putString("CustomName", Component.Serializer.toJson(name, registries));
         }
-        tag.put("Item", stack.saveOptional(registries));
+        if (!stack.isEmpty()) {
+            tag.put("Item", stack.save(registries));
+        }
+        tag.putInt("Etherium", etherium);
+        tag.putInt("BurnTime", burnTime);
+        tag.putInt("MaxBurnTime", maxBurnTime);
+        tag.putInt("EtheriumPerTick", etheriumPerTick);
     }
 
     @Override
@@ -170,5 +218,37 @@ public class ObeliskBlockEntity extends BlockEntity implements MenuProvider, Nam
     @Override
     public void fillStackedContents(StackedContents contents) {
         contents.accountStack(stack);
+    }
+
+    @Override
+    public int getAmount(Holder<EtheriumType> type) {
+        return type.is(AMEtheriumTypes.NEUTRAL) ? etherium : 0;
+    }
+
+    @Override
+    public int getMaxAmount(Holder<EtheriumType> type) {
+        return type.is(AMEtheriumTypes.NEUTRAL) ? AMServerConfig.OBELISK_MAX_ETHERIUM.get() : 0;
+    }
+
+    @Override
+    public void setAmount(Holder<EtheriumType> type, int amount) {
+        if (type.is(AMEtheriumTypes.NEUTRAL)) {
+            etherium = amount;
+            setChanged();
+        }
+    }
+
+    @Override
+    public int addAmount(Holder<EtheriumType> type, int amount) {
+        return amount;
+    }
+
+    @Override
+    public int subtractAmount(Holder<EtheriumType> type, int amount) {
+        if (!type.is(AMEtheriumTypes.NEUTRAL)) return amount;
+        int min = Math.min(etherium, amount);
+        etherium -= min;
+        setChanged();
+        return amount - min;
     }
 }

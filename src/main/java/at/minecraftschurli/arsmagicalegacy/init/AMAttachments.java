2e432728534ca91331b59ff12889ae9dea4a361a
IchHabeHunger54 <kingdavidaut@gmail.com>
2025-11-09 12:48:28 +0100

    fix broken formatter comments

--- src/main/java/at/minecraftschurli/arsmagicalegacy/init/AMAttachments.java
+++ src/main/java/at/minecraftschurli/arsmagicalegacy/init/AMAttachments.java
@@ -12,13 +12,13 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import java.util.function.Supplier;
 
 public interface AMAttachments {
-    // formatter:off
+    // @formatter:off
     DeferredHolder<AttachmentType<?>, AttachmentType<Double>>          BURNOUT = register("burnout", () -> 0.,                      Codec.DOUBLE,          ByteBufCodecs.DOUBLE);
     DeferredHolder<AttachmentType<?>, AttachmentType<Integer>>         FROST   = register("frost",   () -> 0,                       Codec.INT,             ByteBufCodecs.INT);
     DeferredHolder<AttachmentType<?>, AttachmentType<MagicAttachment>> MAGIC   = register("magic",   () -> MagicAttachment.DEFAULT, MagicAttachment.CODEC, MagicAttachment.STREAM_CODEC);
     DeferredHolder<AttachmentType<?>, AttachmentType<Double>>          MANA    = register("mana",    () -> 0.,                      Codec.DOUBLE,          ByteBufCodecs.DOUBLE);
     DeferredHolder<AttachmentType<?>, AttachmentType<TemporalAnchorEffect.Snapshot>> TEMPORAL_ANCHOR_SNAPSHOT = AMRegistries.ATTACHMENTS.register("temporal_anchor_snapshot", () -> AttachmentType.<TemporalAnchorEffect.Snapshot>builder(() -> null).serialize(TemporalAnchorEffect.Snapshot.CODEC).build());
-    // formatter:on
+    // @formatter:on
 
     private static <T> DeferredHolder<AttachmentType<?>, AttachmentType<T>> register(String name, Supplier<T> defaultValueSupplier, Codec<T> codec, StreamCodec<? super RegistryFriendlyByteBuf, T> streamCodec) {
         return AMRegistries.ATTACHMENTS.register(name, () -> AttachmentType.builder(defaultValueSupplier).serialize(codec).sync(streamCodec).copyOnDeath().build());

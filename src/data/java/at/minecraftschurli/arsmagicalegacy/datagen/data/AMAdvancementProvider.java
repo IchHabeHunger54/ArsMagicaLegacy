5292ee8deafe3f7d4f01456d44938410a8a4d48d
IchHabeHunger54 <kingdavidaut@gmail.com>
2026-01-10 16:26:37 +0100

    add skill advancements and chapter locks in the compendium

--- src/data/java/at/minecraftschurli/arsmagicalegacy/datagen/data/AMAdvancementProvider.java
+++ src/data/java/at/minecraftschurli/arsmagicalegacy/datagen/data/AMAdvancementProvider.java
@@ -1,6 +1,9 @@
 package at.minecraftschurli.arsmagicalegacy.datagen.data;
 
 import at.minecraftschurli.arsmagicalegacy.api.ArsMagicaApi;
+import at.minecraftschurli.arsmagicalegacy.api.advancement.SkillChangeTrigger;
+import at.minecraftschurli.arsmagicalegacy.api.constants.AMRegistries;
+import at.minecraftschurli.arsmagicalegacy.init.AMCriterionTriggers;
 import at.minecraftschurli.arsmagicalegacy.init.AMItems;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.advancements.Advancement;
@@ -9,9 +12,15 @@ import net.minecraft.advancements.AdvancementRequirements;
 import net.minecraft.advancements.AdvancementType;
 import net.minecraft.advancements.Criterion;
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
+import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.component.DataComponentPredicate;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
 import net.neoforged.neoforge.common.data.AdvancementProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
@@ -27,14 +36,24 @@ public final class AMAdvancementProvider extends AdvancementProvider {
     }
 
     private static class AdvancementSubProvider implements AdvancementGenerator {
+        @SuppressWarnings({"DataFlowIssue", "unchecked"})
         @Override
         public void generate(HolderLookup.Provider registries, Consumer<AdvancementHolder> saver, ExistingFileHelper existingFileHelper) {
-            AdvancementHolder root = Advancement.Builder.advancement() //TODO use arcane compendium instead
-                .display(AMItems.VINTEUM_DUST, title("root"), description("root"), ArsMagicaApi.modLoc("textures/gui/advancements/background.png"), AdvancementType.TASK, false, false, true)
-                .addCriterion("vinteum_dust", InventoryChangeTrigger.TriggerInstance.hasItems(AMItems.VINTEUM_DUST))
-                .requirements(AdvancementRequirements.allOf(List.of("vinteum_dust")))
+            ItemStack book = ArsMagicaApi.book();
+            DataComponentType<?> bookComponent = BuiltInRegistries.DATA_COMPONENT_TYPE.get(ResourceLocation.fromNamespaceAndPath("patchouli", "book"));
+            Criterion<InventoryChangeTrigger.TriggerInstance> bookCriterion = InventoryChangeTrigger.TriggerInstance.hasItems(ItemPredicate.Builder.item().of(book.getItem()).hasComponents(DataComponentPredicate.builder().<Object>expect((DataComponentType<? super Object>) bookComponent, book.get(bookComponent)).build()));
+            AdvancementHolder bookRoot = Advancement.Builder.advancement()
+                .addCriterion("arcane_compendium", bookCriterion)
+                .save(saver, ArsMagicaApi.modLoc("book/root").toString());
+            registries.lookupOrThrow(AMRegistries.SKILL).listElements().forEach(skill -> Advancement.Builder.advancement()
+                .parent(bookRoot)
+                .addCriterion("knows", AMCriterionTriggers.SKILL_CHANGE.get().createCriterion(new SkillChangeTrigger.TriggerInstance(List.of(skill))))
+                .save(saver, ArsMagicaApi.modLoc("book/" + skill.getKey().location().getPath()).toString()));
+            AdvancementHolder root = Advancement.Builder.advancement()
+                .display(book, title("root"), description("root"), ArsMagicaApi.modLoc("textures/gui/advancements/background.png"), AdvancementType.TASK, false, false, true)
+                .addCriterion("arcane_compendium", bookCriterion)
                 .save(saver, ArsMagicaApi.modLoc("root").toString());
-            advancement(saver, "spell", root, AMItems.SPELL, Pair.of("spell", InventoryChangeTrigger.TriggerInstance.hasItems(AMItems.SPELL)));
+            advancement(saver, "spell", root, AMItems.SPELL, false, Pair.of("spell", InventoryChangeTrigger.TriggerInstance.hasItems(AMItems.SPELL)));
         }
 
         private Component title(String name) {
@@ -47,10 +66,10 @@ public final class AMAdvancementProvider extends AdvancementProvider {
 
         @SuppressWarnings({"SameParameterValue", "UnusedReturnValue"})
         @SafeVarargs
-        private AdvancementHolder advancement(Consumer<AdvancementHolder> saver, String name, AdvancementHolder parent, ItemLike icon, Pair<String, Criterion<?>>... criteria) {
+        private AdvancementHolder advancement(Consumer<AdvancementHolder> saver, String name, AdvancementHolder parent, ItemLike icon, boolean hidden, Pair<String, Criterion<?>>... criteria) {
             Advancement.Builder builder = Advancement.Builder.advancement()
                 .parent(parent)
-                .display(icon, title(name), description(name), null, AdvancementType.TASK, true, true, false);
+                .display(icon, title(name), description(name), null, AdvancementType.TASK, true, true, hidden);
             Arrays.stream(criteria).forEach(criterion -> builder.addCriterion(criterion.getFirst(), criterion.getSecond()));
             builder.requirements(AdvancementRequirements.allOf(Arrays.stream(criteria).map(Pair::getFirst).toList()));
             return builder.save(saver, ArsMagicaApi.modLoc(name).toString());
---
+++ src/main/java/at/minecraftschurli/arsmagicalegacy/init/AMCriterionTriggers.java
@@ -0,0 +1,14 @@
+package at.minecraftschurli.arsmagicalegacy.init;
+
+import at.minecraftschurli.arsmagicalegacy.api.ArsMagicaApi;
+import at.minecraftschurli.arsmagicalegacy.api.advancement.SkillChangeTrigger;
+import net.minecraft.advancements.CriterionTrigger;
+import net.minecraft.core.registries.Registries;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public interface AMCriterionTriggers {
+    DeferredRegister<CriterionTrigger<?>> TRIGGER_TYPES = DeferredRegister.create(Registries.TRIGGER_TYPE, ArsMagicaApi.MOD_ID);
+
+    DeferredHolder<CriterionTrigger<?>, SkillChangeTrigger> SKILL_CHANGE = TRIGGER_TYPES.register("skill_change", SkillChangeTrigger::new);
+}

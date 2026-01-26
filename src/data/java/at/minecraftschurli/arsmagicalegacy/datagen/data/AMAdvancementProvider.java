b60f9e57387dea1b1ef8a143c124fa1e2657f658
IchHabeHunger54 <kingdavidaut@gmail.com>
2026-01-26 03:51:36 +0100

    add affinity advancements

--- src/data/java/at/minecraftschurli/arsmagicalegacy/datagen/data/AMAdvancementProvider.java
+++ src/data/java/at/minecraftschurli/arsmagicalegacy/datagen/data/AMAdvancementProvider.java
@@ -1,37 +1,34 @@
 package at.minecraftschurli.arsmagicalegacy.datagen.data;
 
+import at.minecraftschurli.arsmagicalegacy.advancement.AffinityChangeTrigger;
 import at.minecraftschurli.arsmagicalegacy.advancement.SkillChangeTrigger;
 import at.minecraftschurli.arsmagicalegacy.api.ArsMagicaApi;
 import at.minecraftschurli.arsmagicalegacy.api.constants.AMRegistries;
-import at.minecraftschurli.arsmagicalegacy.api.magic.Skill;
+import at.minecraftschurli.arsmagicalegacy.api.magic.Affinity;
+import at.minecraftschurli.arsmagicalegacy.init.AMDataComponents;
 import at.minecraftschurli.arsmagicalegacy.init.AMItems;
-import at.minecraftschurli.arsmagicalegacy.init.AMSpells;
+import at.minecraftschurli.arsmagicalegacy.init.AMMagic;
+import at.minecraftschurli.arsmagicalegacy.item.DataComponentNamedItem;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementHolder;
-import net.minecraft.advancements.AdvancementRequirements;
 import net.minecraft.advancements.AdvancementType;
 import net.minecraft.advancements.Criterion;
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
 import net.minecraft.advancements.critereon.ItemPredicate;
-import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.component.DataComponentPredicate;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.ItemLike;
 import net.neoforged.neoforge.common.data.AdvancementProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
-import net.neoforged.neoforge.registries.DeferredHolder;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
-import java.util.stream.Stream;
 
 public final class AMAdvancementProvider extends AdvancementProvider {
     public AMAdvancementProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries, ExistingFileHelper existingFileHelper) {
@@ -42,6 +39,7 @@ public final class AMAdvancementProvider extends AdvancementProvider {
         @SuppressWarnings({"DataFlowIssue", "unchecked"})
         @Override
         public void generate(HolderLookup.Provider registries, Consumer<AdvancementHolder> saver, ExistingFileHelper existingFileHelper) {
+            HolderLookup.RegistryLookup<Affinity> affinities = registries.lookupOrThrow(AMRegistries.AFFINITY);
             ItemStack book = ArsMagicaApi.book();
             DataComponentType<?> bookComponent = BuiltInRegistries.DATA_COMPONENT_TYPE.get(ResourceLocation.fromNamespaceAndPath("patchouli", "book"));
             Criterion<InventoryChangeTrigger.TriggerInstance> bookCriterion = InventoryChangeTrigger.TriggerInstance.hasItems(ItemPredicate.Builder.item().of(book.getItem()).hasComponents(DataComponentPredicate.builder().<Object>expect((DataComponentType<? super Object>) bookComponent, book.get(bookComponent)).build()));
@@ -58,16 +56,24 @@ public final class AMAdvancementProvider extends AdvancementProvider {
                 .display(book, title("root"), description("root"), ArsMagicaApi.modLoc("textures/gui/advancements/background.png"), AdvancementType.TASK, false, false, true)
                 .addCriterion("arcane_compendium", bookCriterion)
                 .save(saver, ArsMagicaApi.modLoc("root").toString());
-            AdvancementHolder skill = advancement(saver, "skill", root, AMItems.OCCULUS, AdvancementType.TASK, false,
+            AdvancementHolder skill = advancement(saver, "skill", root, AMItems.OCCULUS.toStack(), AdvancementType.TASK, false,
                 builder -> builder.addCriterion("knows", SkillChangeTrigger.create(SkillChangeTrigger.Requirements.ANY_NON_HIDDEN)));
-            AdvancementHolder allSkills = advancement(saver, "all_skills", skill, AMItems.OCCULUS, AdvancementType.CHALLENGE, false,
+            AdvancementHolder allSkills = advancement(saver, "all_skills", skill, AMItems.OCCULUS.toStack(), AdvancementType.CHALLENGE, false,
                 builder -> builder.addCriterion("knows", SkillChangeTrigger.create(SkillChangeTrigger.Requirements.ALL_NON_HIDDEN)));
-            AdvancementHolder hiddenSkill = advancement(saver, "hidden_skill", skill, AMItems.OCCULUS, AdvancementType.TASK, true,
+            AdvancementHolder hiddenSkill = advancement(saver, "hidden_skill", skill, AMItems.OCCULUS.toStack(), AdvancementType.TASK, true,
                 builder -> builder.addCriterion("knows", SkillChangeTrigger.create(SkillChangeTrigger.Requirements.ANY_HIDDEN)));
-            AdvancementHolder allHiddenSkills = advancement(saver, "all_hidden_skills", hiddenSkill, AMItems.OCCULUS, AdvancementType.CHALLENGE, false,
+            AdvancementHolder allHiddenSkills = advancement(saver, "all_hidden_skills", hiddenSkill, AMItems.OCCULUS.toStack(), AdvancementType.CHALLENGE, false,
                 builder -> builder.addCriterion("knows", SkillChangeTrigger.create(SkillChangeTrigger.Requirements.ALL_HIDDEN)));
-            AdvancementHolder spell = advancement(saver, "spell", skill, AMItems.SPELL, AdvancementType.TASK, false,
+            AdvancementHolder spell = advancement(saver, "spell", skill, AMItems.SPELL.toStack(), AdvancementType.TASK, false,
                 builder -> builder.addCriterion("spell", InventoryChangeTrigger.TriggerInstance.hasItems(AMItems.SPELL)));
+            AdvancementHolder affinityOnePercent = advancement(saver, "affinity_one_percent", spell, DataComponentNamedItem.set(AMItems.AFFINITY_ESSENCE.toStack(), AMDataComponents.AFFINITY.get(), affinities.getOrThrow(AMMagic.WATER)), AdvancementType.TASK, false,
+                builder -> builder.addCriterion("affinity", AffinityChangeTrigger.create(0.01)));
+            AdvancementHolder affinityFiftyPercent = advancement(saver, "affinity_fifty_percent", affinityOnePercent, DataComponentNamedItem.set(AMItems.AFFINITY_ESSENCE.toStack(), AMDataComponents.AFFINITY.get(), affinities.getOrThrow(AMMagic.LIFE)), AdvancementType.TASK, false,
+                builder -> builder.addCriterion("affinity", AffinityChangeTrigger.create(0.5)));
+            AdvancementHolder affinityFull = advancement(saver, "affinity_full", affinityFiftyPercent, DataComponentNamedItem.set(AMItems.AFFINITY_ESSENCE.toStack(), AMDataComponents.AFFINITY.get(), affinities.getOrThrow(AMMagic.ENDER)), AdvancementType.CHALLENGE, false,
+                builder -> builder.addCriterion("affinity", AffinityChangeTrigger.create(1)));
+            AdvancementHolder affinityTome = advancement(saver, "affinity_tome", affinityFull, DataComponentNamedItem.set(AMItems.AFFINITY_TOME.toStack(), AMDataComponents.AFFINITY.get(), affinities.getOrThrow(Affinity.NONE)), AdvancementType.TASK, true,
+                builder -> builder.addCriterion("affinity", AffinityChangeTrigger.create(1, 0)));
         }
 
         private Component title(String name) {
@@ -78,7 +84,7 @@ public final class AMAdvancementProvider extends AdvancementProvider {
             return Component.translatable("advancements." + ArsMagicaApi.MOD_ID + "." + name + ".description");
         }
 
-        private AdvancementHolder advancement(Consumer<AdvancementHolder> saver, String name, AdvancementHolder parent, ItemLike icon, AdvancementType type, boolean hidden, Consumer<Advancement.Builder> consumer) {
+        private AdvancementHolder advancement(Consumer<AdvancementHolder> saver, String name, AdvancementHolder parent, ItemStack icon, AdvancementType type, boolean hidden, Consumer<Advancement.Builder> consumer) {
             Advancement.Builder builder = Advancement.Builder.advancement()
                 .parent(parent)
                 .display(icon, title(name), description(name), null, type, true, true, hidden);

8622a782ea43f7ceb7856f2673e40ef135451103
IchHabeHunger54 <kingdavidaut@gmail.com>
2026-02-08 22:37:35 +0100

    remove TeleportComponent

--- src/main/java/at/minecraftschurli/arsmagicalegacy/spell/component/Blink.java
+++ src/main/java/at/minecraftschurli/arsmagicalegacy/spell/component/Blink.java
@@ -3,9 +3,10 @@ package at.minecraftschurli.arsmagicalegacy.spell.component;
 import at.minecraftschurli.arsmagicalegacy.AMServerConfig;
 import at.minecraftschurli.arsmagicalegacy.api.ArsMagicaApi;
 import at.minecraftschurli.arsmagicalegacy.api.spell.Spell;
+import at.minecraftschurli.arsmagicalegacy.api.spell.SpellComponent;
 import at.minecraftschurli.arsmagicalegacy.api.spell.SpellModifier;
 import at.minecraftschurli.arsmagicalegacy.init.AMSpells;
-import at.minecraftschurli.arsmagicalegacy.spell.TeleportComponent;
+import at.minecraftschurli.arsmagicalegacy.util.AMUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
@@ -16,13 +17,15 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class Blink extends TeleportComponent {
+public class Blink extends SpellComponent.CastEntity {
     public Blink() {
         super(AMSpells.RANGE_STAT);
     }
 
     @Override
-    protected void teleport(Spell spell, List<SpellModifier> modifiers, Level level, @Nullable LivingEntity caster, @Nullable Entity directEntity, EntityHitResult hitResult, Entity entity) {
+    public Spell castEntity(Spell spell, List<SpellModifier> modifiers, Level level, @Nullable LivingEntity caster, @Nullable Entity directEntity, EntityHitResult hitResult) {
+        Entity entity = hitResult.getEntity();
+        if (AMUtil.cancelTeleport(entity, caster)) return spell;
         for (int i = (int) Math.round(ArsMagicaApi.spellHelper().getModifiedStat(AMServerConfig.BLINK_RANGE.get(), AMSpells.RANGE_STAT, modifiers, spell, level, caster, directEntity, hitResult)); i > 0; i--) {
             Vec3 angle = entity.getLookAngle().normalize();
             double x = entity.getX() + angle.x() * i;
@@ -33,5 +36,6 @@ public class Blink extends TeleportComponent {
                 break;
             }
         }
+        return spell;
     }
 }

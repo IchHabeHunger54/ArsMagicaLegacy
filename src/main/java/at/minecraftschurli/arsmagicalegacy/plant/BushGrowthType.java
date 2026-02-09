1ea8eeaf3678c24551ada47ebc02b0c4cc0b08f4
IchHabeHunger54 <kingdavidaut@gmail.com>
2026-02-09 02:40:40 +0100

    add sweet berry bush support

---
+++ src/main/java/at/minecraftschurli/arsmagicalegacy/plant/BushGrowthType.java
@@ -0,0 +1,66 @@
+package at.minecraftschurli.arsmagicalegacy.plant;
+
+import at.minecraftschurli.arsmagicalegacy.api.plant.GrowthContext;
+import at.minecraftschurli.arsmagicalegacy.api.plant.GrowthType;
+import com.mojang.serialization.MapCodec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BonemealableBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.List;
+
+public record BushGrowthType() implements GrowthType {
+    public static final MapCodec<BushGrowthType> CODEC = MapCodec.unit(BushGrowthType::new);
+
+    @Override
+    public MapCodec<? extends GrowthType> codec() {
+        return CODEC;
+    }
+
+    @Override
+    public boolean canGrow(GrowthContext context) {
+        BlockState state = context.state();
+        return !canHarvest(context) && state.getBlock() instanceof BonemealableBlock block && block.isValidBonemealTarget(context.level(), context.pos(), state);
+    }
+
+    @Override
+    public void grow(GrowthContext context) {
+        ServerLevel level = context.level();
+        BlockState state = context.state();
+        if (state.getBlock() instanceof BonemealableBlock block) {
+            block.performBonemeal(level, level.getRandom(), context.pos(), state);
+        }
+    }
+
+    @Override
+    public boolean canHarvest(GrowthContext context) {
+        return context.plant().harvestStates().containsKey(context.state());
+    }
+
+    @Override
+    public List<ItemStack> harvest(GrowthContext context) {
+        BlockPos pos = context.pos();
+        Block.beginCapturingDrops();
+        context.state().useWithoutItem(context.level(), context.player(), new BlockHitResult(Vec3.atCenterOf(pos), Direction.UP, pos, true));
+        return Block.stopCapturingDrops()
+            .stream()
+            .map(ItemEntity::getItem)
+            .toList();
+    }
+
+    @Override
+    public boolean canReplant(GrowthContext context) {
+        return false;
+    }
+
+    @Override
+    public void replant(GrowthContext context) {
+    }
+}

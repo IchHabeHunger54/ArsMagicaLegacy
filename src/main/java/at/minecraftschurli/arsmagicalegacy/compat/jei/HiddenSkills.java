c7c7fee9e81c3a93682ef2aa9b6a7343ef18ab6e
IchHabeHunger54 <kingdavidaut@gmail.com>
2026-02-18 22:02:04 +0100

    not-fully-working impl of hiding skills themselves from JEI

--- src/main/java/at/minecraftschurli/arsmagicalegacy/compat/jei/HiddenSkills.java
+++ src/main/java/at/minecraftschurli/arsmagicalegacy/compat/jei/HiddenSkills.java
@@ -8,10 +8,12 @@ import at.minecraftschurli.arsmagicalegacy.util.AMClientUtil;
 import mezz.jei.api.recipe.IFocusFactory;
 import mezz.jei.api.recipe.IRecipeManager;
 import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.runtime.IIngredientManager;
 import mezz.jei.api.runtime.IJeiRuntime;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.Holder;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
@@ -35,6 +37,13 @@ public final class HiddenSkills {
             .filter(e -> AMRegistries.SPELL_PARTS.containsKey(e.getKey().location()))
             .sorted(Comparator.comparing(e -> Skill.getName(e).getString()))
             .toList();
+        IIngredientManager ingredientManager = runtime.getIngredientManager();
+        ingredientManager.removeIngredientsAtRuntime(AMJeiPlugin.SKILL_TYPE, new ArrayList<>(ingredientManager.getAllIngredients(AMJeiPlugin.SKILL_TYPE)));
+        List<Skill> skills = list.stream()
+            .filter(e -> !e.value().hidden() || helper.knows(player, e))
+            .map(Holder::value)
+            .toList(); // fixme
+        ingredientManager.addIngredientsAtRuntime(AMJeiPlugin.SKILL_TYPE, skills);
         IRecipeManager recipeManager = runtime.getRecipeManager();
         recipeManager.unhideRecipes(SkillCategory.RECIPE_TYPE, HIDDEN_RECIPES);
         HIDDEN_RECIPES.clear();

9a2cd4512813b3a726f42c33e2f9a4d1d00ca236
Minecraftschurli <minecraftschurli@gmail.com>
2026-03-15 00:44:29 +0100

    Port spell model

--- src/main/java/at/minecraftschurli/arsmagicalegacy/client/atlas/SpellIconAtlasHolder.java
+++ src/main/java/at/minecraftschurli/arsmagicalegacy/client/atlas/SpellIconAtlasHolder.java
@@ -4,6 +4,8 @@ import at.minecraftschurli.arsmagicalegacy.api.ArsMagicaApi;
 import at.minecraftschurli.arsmagicalegacy.util.AMClientUtil;
 import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.sprite.SpriteGetter;
+import net.minecraft.client.resources.model.sprite.SpriteId;
 import net.minecraft.resources.Identifier;
 import org.jspecify.annotations.Nullable;
 
@@ -16,6 +18,22 @@ public class SpellIconAtlasHolder {
     public static TextureAtlasSprite getSprite(@Nullable Identifier location) {
         TextureAtlas atlas = AMClientUtil.mc().getAtlasManager().getAtlasOrThrow(ATLAS_ID);
         return location == null ? atlas.missingSprite() : atlas.getSprite(location);
+    }
+
+    public static TextureAtlasSprite getSprite(SpriteGetter getter, Identifier location) {
+        return getter.get(new SpriteId(ATLAS, location));
+    }
+
+    public static @Nullable TextureAtlasSprite getSpriteOrNull(SpriteGetter getter, Identifier location) {
+        TextureAtlasSprite sprite = getSprite(getter, location);
+        if (sprite == getMissingSprite()) {
+            return null;
+        }
+        return sprite;
+    }
+
+    public static TextureAtlasSprite getMissingSprite() {
+        return AMClientUtil.mc().getAtlasManager().getAtlasOrThrow(ATLAS_ID).missingSprite();
     }
 
     public static Collection<Identifier> getIcons() {

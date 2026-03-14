0fca81d10f17c13505b2282a5628c1714f1fdaaa
IchHabeHunger54 <kingdavidaut@gmail.com>
2026-03-14 16:54:42 +0100

    fix loot table objects

--- src/main/java/at/minecraftschurli/arsmagicalegacy/loot/EnchantmentLevelFromItemProvider.java
+++ src/main/java/at/minecraftschurli/arsmagicalegacy/loot/EnchantmentLevelFromItemProvider.java
@@ -1,16 +1,14 @@
 package at.minecraftschurli.arsmagicalegacy.loot;
 
-import at.minecraftschurli.arsmagicalegacy.init.AMLoot;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.Holder;
+import net.minecraft.util.context.ContextKey;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.LevelBasedValue;
 import net.minecraft.world.level.storage.loot.LootContext;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParam;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
-import net.minecraft.world.level.storage.loot.providers.number.LootNumberProviderType;
 import net.minecraft.world.level.storage.loot.providers.number.NumberProvider;
 
 import java.util.Set;
@@ -23,18 +21,18 @@ public record EnchantmentLevelFromItemProvider(Holder<Enchantment> enchantment, 
 
     @Override
     public float getFloat(LootContext lootContext) {
-        if (!lootContext.hasParam(LootContextParams.DAMAGE_SOURCE)) return 0;
-        ItemStack weapon = lootContext.getParam(LootContextParams.DAMAGE_SOURCE).getWeaponItem();
+        if (!lootContext.hasParameter(LootContextParams.DAMAGE_SOURCE)) return 0;
+        ItemStack weapon = lootContext.getParameter(LootContextParams.DAMAGE_SOURCE).getWeaponItem();
         return weapon != null ? value.calculate(weapon.getEnchantmentLevel(enchantment)) : 0;
     }
 
     @Override
-    public LootNumberProviderType getType() {
-        return AMLoot.ENCHANTMENT_LEVEL_FROM_ITEM.get();
+    public MapCodec<? extends NumberProvider> codec() {
+        return CODEC;
     }
 
     @Override
-    public Set<LootContextParam<?>> getReferencedContextParams() {
+    public Set<ContextKey<?>> getReferencedContextParams() {
         return Set.of(LootContextParams.DAMAGE_SOURCE);
     }
 }

f3937dd0fd975d7c17b0fec8e91426d7132cc475
IchHabeHunger54 <kingdavidaut@gmail.com>
2026-04-03 23:47:54 +0200

    add boss sounds

--- src/main/java/at/minecraftschurli/mods/arsmagicalegacy/entity/ArcaneGuardian.java
+++ src/main/java/at/minecraftschurli/mods/arsmagicalegacy/entity/ArcaneGuardian.java
@@ -1,6 +1,9 @@
 package at.minecraftschurli.mods.arsmagicalegacy.entity;
 
 import at.minecraftschurli.mods.arsmagicalegacy.init.AMAttributes;
+import at.minecraftschurli.mods.arsmagicalegacy.init.AMSounds;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -17,5 +20,25 @@ public class ArcaneGuardian extends AbstractBoss {
             .add(Attributes.ARMOR, 10)
             .add(AMAttributes.MAX_MANA, 2000)
             .add(AMAttributes.MAX_BURNOUT, 2000);
+    }
+
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return AMSounds.ARCANE_GUARDIAN_AMBIENT.get();
+    }
+
+    @Override
+    public SoundEvent getAttackSound() {
+        return AMSounds.ARCANE_GUARDIAN_ATTACK.get();
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return AMSounds.ARCANE_GUARDIAN_DEATH.get();
+    }
+
+    @Override
+    protected SoundEvent getHurtSound(DamageSource source) {
+        return AMSounds.ARCANE_GUARDIAN_HURT.get();
     }
 }

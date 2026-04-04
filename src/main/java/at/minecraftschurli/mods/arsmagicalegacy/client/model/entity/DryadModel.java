897ef545ee56e62f22b914e133a26bb4f2c5f2d1
IchHabeHunger54 <kingdavidaut@gmail.com>
2026-04-04 02:25:59 +0200

    rename AMModelLayers to AMModels

--- src/main/java/at/minecraftschurli/mods/arsmagicalegacy/client/model/entity/DryadModel.java
+++ src/main/java/at/minecraftschurli/mods/arsmagicalegacy/client/model/entity/DryadModel.java
@@ -1,16 +1,12 @@
 package at.minecraftschurli.mods.arsmagicalegacy.client.model.entity;
 
-import at.minecraftschurli.mods.arsmagicalegacy.api.ArsMagicaApi;
 import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.builders.CubeDeformation;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
 
 public class DryadModel extends HumanoidModel<HumanoidRenderState> {
-    public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(ArsMagicaApi.id("dryad"), "main");
-
     public DryadModel(ModelPart root) {
         super(root);
     }
